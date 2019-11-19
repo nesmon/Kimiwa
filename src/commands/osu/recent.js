fd9d381e6b91a5d55032b3650ad4bdc556b245be
Nesmon <nesmon2108@protonmail.com>
2019-11-19 09:06:48 +0100

    update rs

--- src/commands/osu/recent.js
+++ src/commands/osu/recent.js
@@ -39,9 +39,24 @@ class Recent extends Command {
 
         let getRecent = await kimiwaHelper.osuAPI(kimiwa, 'getRecent', name, mode, 1);
         let getBeatmap = await kimiwaHelper.osuAPI(kimiwa, 'getBeatpmapId', getRecent[0].beatmap_id);
+        let renderBeatmapName = getBeatmap[0].artist + "-" + getBeatmap[0].title + "[" + getBeatmap[0].version + "]";
+
+        let getMap = await kimiwaHelper.getOsuBeatmapCache(getBeatmap[0].beatmap_id);
+        let parseBeatmap = new kimiwaHelper.ojsama.parser();
+        parseBeatmap.feed(getMap);
+
+        let beatmap = parseBeatmap.map;
+        let beatmapStars = await new kimiwaHelper.ojsama.diff().calc({
+            map: beatmap,
+            mods: parseInt(getBeatmap[0].enabled_mods)
+        });
+        beatmapStars = beatmapStars.toString().split(" ", 1)[0];
+        let beatmapUsedMods = (kimiwaHelper.getModByNumber(getBeatmap[0].enabled_mods).length > 0) ? "+" + kimiwaHelper.getModByNumber(getBeatmap[0].enabled_mods).join(',') : "Nomod";
+
 
         message.channel.createEmbed(new kimiwaHelper.Embed()
             .setColor(16016293)
+            .setTitle(`${renderBeatmapName}]+${beatmapUsedMods}`)
             .setDescription("test")
         );
         console.log(getRecent);
