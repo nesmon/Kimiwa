7337d6eb0756ad8296e91eee8c58d7fb739d871c
Nesmon <nesmon2108@protonmail.com>
2019-11-21 00:10:17 +0100

    I cant continue, I think i juste make manually the calcull

--- src/commands/osu/recent.js
+++ src/commands/osu/recent.js
@@ -77,8 +77,8 @@ class Recent extends Command {
             ) * 300)) * 100));
 
         console.log(acc);
-        let beatmapPP = new kimiwaHelper.ojsama.ppv2({stars: beatmapStars, max_combo: Number(getRecent[0].maxcombo), nmiss: Number(getRecent[0].countmiss), acc_percent: acc });
-        let beatmapppforacc = new kimiwaHelper.ojsama.ppv2({stars: beatmapStars, max_combo: Number(getBeatmap[0].max_combo), nmiss: 0, acc_percent: accIfFC });
+        let beatmapPP = kimiwaHelper.ojsama.ppv2({stars: beatmapStars, max_combo: Number(getRecent[0].maxcombo), nmiss: Number(getRecent[0].countmiss), acc_percent: acc });
+        let beatmapppforacc = kimiwaHelper.ojsama.ppv2({stars: beatmapStars, max_combo: Number(getBeatmap[0].max_combo), nmiss: 0, acc_percent: accIfFC });
         let ppIfFC = beatmapppforacc.toString().split(" ", 1)[0];
         let PPmin = beatmapPP.toString().split(" ", 1)[0];
         console.log(beatmapPP.toString());
