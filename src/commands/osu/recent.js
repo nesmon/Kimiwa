958c01673383077dc121a81a23f525b52d2956a4
Nesmon <nesmon2108@protonmail.com>
2019-11-20 22:54:31 +0100

    I cant continue, I think i juste make manually the calcull

--- src/commands/osu/recent.js
+++ src/commands/osu/recent.js
@@ -53,7 +53,7 @@ class Recent extends Command {
             map: beatmap,
             mods: parseInt(getBeatmap[0].enabled_mods)
         });
-        console.log(beatmapStars);
+        console.log(beatmapStars.toString());
         beatmapStars = beatmapStars.toString().split(" ", 1)[0];
         let beatmapUsedMods = (kimiwaHelper.getModByNumber(getBeatmap[0].enabled_mods).length > 0) ? "+" + kimiwaHelper.getModByNumber(getBeatmap[0].enabled_mods).join(',') : "Nomod";
 
