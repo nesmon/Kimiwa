ea6b770ca254fc788aa9aff08b7f5c99b9a815e7
Nesmon <nesmon2108@protonmail.com>
2019-11-29 23:21:03 +0100

    Rebuilt embed of osu cmd

--- src/extensions/OsuHelper.js
+++ src/extensions/OsuHelper.js
@@ -2,7 +2,7 @@ const kimiwaHelper = require('./../kimiwaHelper');
 const ojsama = require('ojsama');
 
 class OsuHelper {
-    async getRangePP(osuUser, kimiwa) {
+    async getRangePP(osuUser, kimiwa, mode) {
         let getBest = await kimiwa.osu.user.getBest(osuUser.user_id, kimiwaHelper.osuGetMode(mode), 100, 'id');
         for (let i = 0; i < getBest.length; i++) {
             console.log(getBest[i].beatmap_id);
