7a3f10840a5526e1b420df8e77d4bbbf8bc90e47
Nesmon <nesmon2108@protonmail.com>
2019-12-15 21:09:59 +0100

    Start get some information for all value need for make custom pp user

--- src/commands/osu/osu.js
+++ src/commands/osu/osu.js
@@ -48,10 +48,9 @@ class Osu extends Command {
 
         const getRangeOsuUser = await kimiwaHelper.getRangeOsuUser(getBest, kimiwa);
 
-        console.log(getRangeOsuUser);
+        console.log(getRangeOsuUser[5]);
 
         let maxcombo = getRangeOsuUser[5] + getRangeOsuUser[6] + getRangeOsuUser[7] + getRangeOsuUser[8];
-        console.log(maxcombo);
         let slider = Number(maxcombo * 35 / 100);
         let circle = Number(maxcombo - slider);
 
